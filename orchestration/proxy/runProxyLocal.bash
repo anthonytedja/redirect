ffdd1245f34b73f3ad86edb7bd0125aba6284229
Kevin Shin <shinkevi@dh2026pc16.utm.utoronto.ca>
2023-10-07 20:46:21 -0400

    Fix proxy shutdown when calling ./dostuff.bash

--- orchestration/proxy/runProxyLocal.bash
+++ orchestration/proxy/runProxyLocal.bash
@@ -4,7 +4,7 @@ OUTDIR="./out"
 cd orchestration/proxy
 rm -rf $OUTDIR
 mkdir -p $OUTDIR
-make build
+make build > /dev/null
 
 java proxy.SimpleProxyServer $PROXYPORT > $OUTDIR/SimpleProxyServer.out 2> $OUTDIR/SimpleProxyServer.err &
 echo $! >&1
