c7c5a0d3c427c3aa51c5d27a22a5387c51ec2e84
Kevin Shin <shinkevi@dh2026pc16.utm.utoronto.ca>
2023-10-07 21:41:42 -0400

    Improve accuracy of server monitoring

--- monitoring/checkServer.bash
+++ monitoring/checkServer.bash
@@ -2,15 +2,15 @@
 # usage: <host>
 # Check if a program called "URLShortner" is running on the specified node and output result to stdout.
 
-grep_name="[U]RLShortner"
-program_name=URLShortner
+grep_name="[U]RLShortnerOptimized"
+program_name=URLShortnerOptimized
 
 red=$(tput setaf 1)
 green=$(tput setaf 2)
 normal=$(tput sgr0)
 
 name=$(ssh $1 "ps aux | grep $grep_name | awk '{print \$14}'" 2>/dev/null)
-if [ "$name" = "$program_name" ] || [ "$name" = "$program_name.java" ]
+if [[ "$name" = *"$program_name"* ]]
 then
 	echo "(${green}o${normal}) server live"
 else
