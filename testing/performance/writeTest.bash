d7630242d9493fbe614ed7398858d3d9d39baf24
Peter Albu <peter.albu@mail.utoronto.ca>
2023-10-09 21:46:03 -0400

    Redirect read/writeTest to folder

--- testing/performance/writeTest.bash
+++ testing/performance/writeTest.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Running ab with $1 requests"
-ab -n $1 -c 4 -u /dev/null -g writeTest.tsv "http://localhost:8000/?short=abc&long=def"
+ab -n $1 -c 4 -u /dev/null -g ./testing/plotting/write/writeTest.tsv "http://localhost:8000/?short=abc&long=def"
